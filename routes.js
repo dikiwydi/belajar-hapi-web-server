d6a74a5dab5a965ca5b973a36235e950734544c9
dikiwydi <dikiwahyudi268@gmail.com>
2023-06-27 07:37:01 +0700

    method/verb request dan routes

--- routes.js
+++ routes.js
@@ -1,61 +1,38 @@
 const routes = [
-    {
-        method: "GET",
-        path: "/",
-        handler: (request, h) => {
-            return "home page";
-        },
+  {
+    method: "GET",
+    path: "/",
+    handler: (request, h) => {
+      return `home page `;
     },
-    {
-        method: "*",
-        path: "/",
-        handler: (request, h) => {
-            return "maaf halam tidak bisa di akses dengan method tersbut";
-        },
+  },
+  {
+    method: "*",
+    path: "/",
+    handler: (request, h) => {
+      return `halaman tidak bisa di akses dengan method tersebut`;
     },
-    {
-        method: "GET",
-        path: "/about",
-        handler: (request, h) => {
-            return "about page";
-        },
+  },
+  {
+    method: "GET",
+    path: "/about",
+    handler: (request, h) => {
+      return `halaman about`;
     },
-    {
-        method: "*",
-        path: "/about",
-        handler: (request, h) => {
-            return "halman tidak bisa di akses dengan method tersebut";
-        },
+  },
+  {
+    method: "*",
+    path: "/about",
+    handler: (request, h) => {
+      return `halaman about tidak bisa di akses dengan mehtod tersebut`;
     },
-    {
-        method: "GET",
-        path: "/hello/{username?}",
-        handler: (request, h) => {
-            const { username = "stranger" } = request.params;
-            const { lang } = request.query;
-
-            if (lang === "id") {
-                return `hai ${username}`;
-            } else {
-                return `hallo ${username}`;
-            }
-        },
+  },
+  {
+    method: "*",
+    path: "/{any*}",
+    handler: (request, h) => {
+      return `halaman tidak ada`;
     },
-    {
-        method: "POST",
-        path: "/login",
-        handler: (request, h) => {
-            const { username, password } = request.payload;
-            return `welcome ${username}`;
-        },
-    },
-    {
-        method: "*",
-        path: "/{any*}",
-        handler: (request, h) => {
-            return "halaman tidak di temukan";
-        },
-    },
+  },
 ];
-
 module.exports = routes;
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const Hapi = require("@hapi/hapi");
-
+const routes = require("./routes");
 const init = () => {
   const server = Hapi.server({
     port: 5000,
     host: "localhost",
   });
+  server.route(routes);
   server.start();
   console.log(`server berjalan pada ${server.info.uri}`);
 };
+init();
